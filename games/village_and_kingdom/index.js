f63637efe9121670c260599e77be6f8c94204624
RoomGames <roomgames.github.io@gmail.com>
2023-06-29 16:53:13 +0300

    update vak

--- games/village_and_kingdom/index.js
+++ games/village_and_kingdom/index.js
@@ -12,9 +12,65 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 console.log(canvas);
 console.log(context);
 
-let image = new Image();
-image.src = "./resources/map.png";
+let mapImage = new Image();
+mapImage.src = "./resources/map.png";
 
-image.onload = () => {
-    context.drawImage(image, 0, 0);
-};
+let playerImage1 = new Image();
+playerImage1.src = "./resources/playerDown.png";
+
+let playerImage2 = new Image();
+playerImage2.src = "./resources/playerLeft.png";
+
+let playerImage3 = new Image();
+playerImage3.src = "./resources/playerRight.png";
+
+let playerImage4 = new Image();
+playerImage4.src = "./resources/playerUp.png";
+
+let backgroundImagePosX = -800;
+let backgroundImagePosY = -400;
+
+class Sprite
+{
+    constructor(position)
+    {
+        this.position = position;
+    }
+}
+
+function animate()
+{
+    window.requestAnimationFrame(animate);
+    context.drawImage(mapImage, backgroundImagePosX, backgroundImagePosY);
+    context.drawImage(playerImage1, 
+        0,
+        0,
+        playerImage1.width / 4,
+        playerImage1.height,
+        canvas.width / 2 - playerImage1.width / 8, 
+        canvas.height / 2 - playerImage1.height / 2,
+        playerImage1.width / 4,
+        playerImage1.height
+    );
+}
+
+animate();
+
+let keyState = {};
+
+document.addEventListener('keydown', function(event)
+{
+    keyState[event.key] = true;
+    console.log('按下键码：' + event.key);
+});
+
+document.addEventListener('keyup', function(event)
+{
+    keyState[event.key] = false;
+    console.log('释放键码：' + event.key);
+});
+
+function checkKeyState(key)
+{
+    return keyState[key] === true;
+}
